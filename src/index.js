3a080a33a1d63441abec3026b357789a98aa2447
AEP <91467886+TukangM@users.noreply.github.com>
2026-03-04 01:40:30 +0700

    gh_pages push

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import './styles/style.css';
-
-// Import images so webpack processes and emits them to dist/img/
-import csbanner from './img/csbanner.gif';
-import chrome from './img/chrome.gif';
-import buttonDevils from './img/button-devils.gif';
